96545cf7a7f635130873c9ba42d23011f9007c64
Registech <registech@laposte.net>
2017-03-07 16:13:59 +0100

    first commit

---
+++ variable.php
@@ -0,0 +1,26 @@
+<?php
+
+$name="Regis";
+echo $name;
+
+$nom="Tech";
+$prenom="Regis";
+$age= 42;
+echo $prenom.$age;
+
+$kilometre= 1;
+echo $kilometre;
+$kilometre= 3;
+echo $kilometre;
+$kilometre= 125;
+echo $kilometre;
+
+$string="String";
+$entier= 45;
+$boolean= true;
+$float= 3.1415927;
+echo $string.$entier.$boolean.$float;
+
+
+
+?>
